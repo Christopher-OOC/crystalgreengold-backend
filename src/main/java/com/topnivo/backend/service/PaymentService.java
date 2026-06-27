3e410ef111cd035799fbaef111bcaefef0c6ab95
Christopher-OOC <olojedechristopher24@gmail.com>
2026-06-27 08:10:50 +0100

    commit

--- src/main/java/com/topnivo/backend/service/PaymentService.java
+++ src/main/java/com/topnivo/backend/service/PaymentService.java
@@ -403,8 +403,7 @@ public class PaymentService {
     }
 
     private void transferToStoreOwner(Member store, double amount) {
-        if (!paystackVerifyPayment) {
-
+        if (paystackVerifyPayment) {
 
             TransferRecord transferRecord = new TransferRecord();
             transferRecord.setReason("You have a Topnivo purchase of " + amount);
